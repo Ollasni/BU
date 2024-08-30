357b5ce1bcb7eac41f9483095f861c1431173d6f
kkontras <konstantinoskontras@gmail.com>
2024-08-30 17:46:31 +0200

    Init Commit

--- print_results.sh
+++ print_results.sh
@@ -1,11 +1,24 @@
 #AVE
 
-python print_search_mean.py --config "./configs/AVE/res/MLB.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --alpha 0.5
-#python print_search_mean.py --config "./configs/AVE/res/singleloss_OGM.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --alpha 0.2
-#python print_search_mean.py --config "./configs/AVE/res/singleloss_AGM.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --alpha 0.5
-#python print_search_mean.py --config "./configs/AVE/res/singleloss_PMR.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --alpha 0.5
-#python print_search_mean.py --config "./configs/AVE/res/singleloss_MMCosine.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --mmcosine_scaling 1
-#python print_search_mean.py --config "./configs/AVE/res/singleloss_MSLR.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --ilr_c 0.9 --ilr_g 1.1 --kmepoch 5
-
+#python print_search_mean.py --config "./configs/AVE/res/MLB.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --alpha 2.0
+#python print_search_mean.py --config "./configs/AVE/res/OGM.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --alpha 0.8
+#python print_search_mean.py --config "./configs/AVE/res/AGM.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --alpha 1.0
+#python print_search_mean.py --config "./configs/AVE/res/PMR.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --alpha 1.5
+#python print_search_mean.py --config "./configs/AVE/res/MMCosine.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --mmcosine_scaling 8
+#python print_search_mean.py --config "./configs/AVE/res/MSLR.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --ilr_c 0.7 --ilr_g 1.3 --kmepoch 10
 #python print_search_mean.py --config "./configs/AVE/res/multiloss.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001
 #python print_search_mean.py --config "./configs/AVE/res/singleloss.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001
+#python print_search_mean.py --config "./configs/AVE/res/audio.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001
+#python print_search_mean.py --config "./configs/AVE/res/video.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001
+
+#python print_search_mean.py --config "./configs/AVE/res/multiloss.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --cls nonlinear
+#python print_search_mean.py --config "./configs/AVE/res/singleloss.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --cls nonlinear
+#python print_search_mean.py --config "./configs/AVE/res/MLB.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --alpha 1.0 --cls nonlinear
+#python print_search_mean.py --config "./configs/AVE/res/AGM.json" --default_config "./configs/AVE/default_config_ave.json" --fold 0 --lr 0.001 --wd 0.0001 --alpha 2.0 --cls nonlinear
+
+
+#UCF101
+
+#MOSEI
+
+#CREMA-D
